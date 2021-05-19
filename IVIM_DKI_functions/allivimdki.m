1b578d3461ddd9218f3b99dc44d3af8bbd2e6f51
amitvmehndiratta <66351266+amitvmehndiratta@users.noreply.github.com>
2021-05-19 14:53:05 +0530

    Delete IVIM_DKI_functions directory

--- IVIM_DKI_functions/allivimdki.m
+++
@@ -1,7 +0,0 @@
-function v= allivimdki( Dp, xdata )   
-% IVIM-DKI hybrid model 
-% D, D*, f and K are calculated by non-linear curve fitting
-% Four free parameter
-   Bk=exp(Dp);
-   v=Bk(3)*exp( -xdata*(Bk(2)) ) + (1-Bk(3))*exp( -xdata*Bk(1)+((((xdata*Bk(1))).^2)*Bk(4))/6 );
-end
--- IVIM_DKI_functions/monoexplog.m
+++
@@ -1,8 +0,0 @@
-function f = monoexplog( Dm, b )  % Dm = ln(ADC)
-% Monoexponential model
-%   ADC parameter is unknown and free parameter
-     C=exp(Dm);
-     f=exp((-b).*C(1));
-     f=f./f(1);
-end
-
