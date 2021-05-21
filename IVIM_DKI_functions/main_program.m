f145e059447a939c78928fb899453044f56e9ad3
amitvmehndiratta <66351266+amitvmehndiratta@users.noreply.github.com>
2021-05-21 06:31:38 +0530

    Update main_program.m

--- IVIM_DKI_functions/main_program.m
+++ IVIM_DKI_functions/main_program.m
@@ -51,23 +51,47 @@ pz=logical(imrotate(pz,90));
 %b-values 
  b=[0 25 50 75 100 150 200 500 800 1000 1250 1500 2000];
  b_mono=[0 500 800 1000];
+ 
 % To perform statistics
  stats=true;
+
+% Select ROI
+roi=tumor;
+
 % Initial values, LB and UB of all pamameters
-% Monoexp model
- D0_mono = log(0.000729); 
- limit_mono= [log(0.0001);log(0.05)];
 % Hybrid model and Hybrid model with TV
  D0_hy = [log(0.000729), log(0.013), log(0.1), log(1.19)]; 
  limit_hy= [log(0.0001), log(0.0001), log(0.001), log(0.01);...
     log(0.05), log(0.5), log(1), log(3)];
+
 %TV parameters(alpha, const and TV iteration)
  alpha=0.005;
  const=0.99;
  TViter=10;
 %% Codes to run
- [paraMap,resnorm,stats_roi]=hyModel(dwiData,b,limit_hy,D0_hy,tumor,stats);
- [paraMapTV,resnormTV,stats_roiTV]=hyModelTV(dwiData,b,limit_hy,D0_hy,tumor,...
+ [paraMapHY,resnorm,stats_roi] = hyModel(dwiData,b,limit_hy,D0_hy,tumor,stats);
+ 
+ [paraMapHYTV,resnormTV,stats_roiTV] = hyModelTV(dwiData,b,limit_hy,D0_hy,tumor,...
     stats,TViter,alpha,const);
-%% Save workspace
- save (strcat(path,'allmodel_results.mat'))
+%% Save parameter maps in compressed nifti format
+mkdir(strcat(path,'Output'))
+
+% Save parameters from HY model
+save_untouch_niigz(imrotate(paraMap.DmapHY,-90),...
+    strcat(path,'Output\Dmap_HY.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMap.DpmapHY,-90),...
+    strcat(path,'Output\Dpmap_HY.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMap.fmapHY,-90),...
+    strcat(path,'Output\fmap_HY.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMap.kmapHY,-90),...
+    strcat(path,'Output\kmap_HY.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+
+% Save parameters from HY model with TV
+save_untouch_niigz(imrotate(paraMapTV.DmapHYtv,-90),...
+    strcat(path,'Output\Dmap_HYtv.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMapTV.DpmapHYtv,-90),...
+    strcat(path,'Output\Dpmap_HYtv.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMapTV.fmapHYtv,-90),...
+    strcat(path,'Output\fmap_HYtv.nii.gz'),strcat(path,'ivim13b.nii.gz'))
+save_untouch_niigz(imrotate(paraMapTV.kmapHYtv,-90),...
+    strcat(path,'Output\kmap_HYtv.nii.gz'),strcat(path,'ivim13b.nii.gz'))
